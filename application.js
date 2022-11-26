639c08499b0bf72013a876d63610e7cb9e8f8438
Kimberly Laughton <kimberlyclaughton@gmail.com>
2022-11-26 16:10:38 -0500

    Made changes to be handed over to alexia

--- application.js
+++ application.js
@@ -6,24 +6,15 @@ window.onload = function() {
     const Lname = document.getElementById('Lname');
     const midinitial = document.getElementById('midtial');
     const ID = document.getElementById('ID');
-    const gender = document.querySelector('input[name="Gender"]:checked');
+    const gender = document.getElementsByName("gender");
     const DOB = document.getElementById('Dateofbirth');
     const nationality = document.getElementById('nation');
-    const single = document.getElementById('single');
-    const married = document.getElementById('married');
-    const divorced = document.getElementById('divorced');
-    const widow = document.getElementById('widow');
-    const commonlaw = document.getElementById('common');
+    const marriage = document.getElementsByName('marStatus');
     const tele = document.getElementById('telnum');
     const homeaddress = document.getElementById('home');
     const mailaddress = document.getElementById('mail');
     const email = document.getElementById('email');
-    const singlepar = document.getElementById('singleparent');
-    const nuclear = document.getElementById('nuclear');
-    const extended = document.getElementById('extended');
-    const sibling = document.getElementById('sibling');
-    const childyes = document.getElementById('childrenyes');
-    const childrenno = document.getElementById('childrenno');
+    const famType = document.getElementsByName('famtype');
     const contactname1 = document.getElementById('contname1');
     const contrelationship = document.getElementById('relationship');
     const contaddress = document.getElementById('contaddress');
@@ -31,26 +22,17 @@ window.onload = function() {
     const contnumber = document.getElementById('contnumber');
     const contname2 = document.getElementById('contname2');
     const contrelationship2 = document.getElementById('relationship2');
+    const contemail2 = document.getElementById('contemail2');
     const contaddress2 = document.getElementById('conthome2');
     const contnumber2 = document.getElementById('contnumber2');
-    const undergrad = document.getElementById('undergrad');
-    const grad = document.getElementById('grad');
-    const lawschool = document.getElementById('lawchool');
-    const firstyear = document.getElementById('first');
-    const secondyear = document.getElementById('second');
-    const thirdyear = document.getElementById('third');
-    const fourthyear = document.getElementById('fourth');
-    const finalyear = document.getElementById('final');
+    const levelstudy = document.getElementsByName('levelofstudy');
+    const studyyear = document.getElementsByName('yearofstudy');
     const programme = document.getElementById('programme');
-    const faculty = document.getElementById('fac');
-    const firstyes = document.getElementById('firstgenyes');
-    const firstno = document.getElementById('firstgenno');
-    const amenities = document.getElementById('yesshare');
-    const amenities2 = document.getElementById('noshare');
+    const fac = document.getElementById('faculty');
+    const firstgen = document.getElementsByName('firstgenstudent');
+    const amenities = document.getElementsByName('accquest1');
     const room = document.getElementById('roomtype');
-    const malepref = document.getElementById('malepref');
-    const femalepref = document.getElementById('femalepref');
-    const anypref = document.getElementById('anypref');
+    const genderpref = document.getElementsByName('gendpref');
     const mate = document.getElementById('matename');
     const arrive = document.getElementById('DOA');
     const football = document.getElementById('football');
@@ -82,12 +64,81 @@ window.onload = function() {
         Middle Initial(s): ${midinitial.value}<br>
         Last Name: ${Lname.value}<br>
         Identification Number: ${ID.value}<br>`
-        review.innerHTML += "Gender: "+gender.value
-       
+        gender.forEach(genderitem => {
+            if(genderitem.checked){
+                review.innerHTML += "Gender: "+genderitem.value + "<br>";
+            }
+            
+        });
         
+        review.innerHTML += `Date of Birth: ${DOB.value}<br>
+        Nationality: ${nationality.value}<br>`
+        marriage.forEach(maritem => {
+            if(maritem.checked){
+                review.innerHTML += "Marital Status: "+maritem.value + "<br>";
+            }
+            
+        });
 
-
+        review.innerHTML += `Telephone number: ${tele.value}<br>
+        Home Address: ${homeaddress.value}<br>
+        Mailing Address: ${mailaddress.value}<br>
+        Email Address: ${email.value}<br>`
+        famType.forEach(famitem => {
+            if(famitem.checked){
+                review.innerHTML += "Marital Status: "+famitem.value + "<br>";
+            }
+            
+        });
+        review.innerHTML += `Contact name: ${contactname1.value}<br>
+        Relationship: ${contrelationship.value}<br>
+        Conatct Address: ${contaddress.value}<br>
+        Email Address: ${contemail.value}<br>
+        Contact number: ${contnumber.value}<br>
+        Contact name: ${contname2.value}<br>
+        Relationship: ${contrelationship2.value}<br>
+        Contact Address: ${contaddress2.value}<br>
+        Email Address: ${contemail2.value}<br>
+        Contact number: ${contnumber2.value}<br>`
+        levelstudy.forEach(level => {
+            if(level.checked){
+                review.innerHTML += "Level of Study: "+level.value + "<br>";
+            }
+            
+        });
+        studyyear.forEach(year => {
+            if(year.checked){
+                review.innerHTML += "Year of Study: "+year.value + "<br>";
+            }
+            
+        });
+        review.innerHTML += `Programme name: ${programme.value}<br>`
+        review.innerHTML +=  "Faculty: " +fac.options[fac.selectedIndex].text + "<br>"
+        firstgen.forEach(item => {
+            if(item.checked){
+                review.innerHTML += "First Generation Student: "+item.value + "<br>";
+            }
+            
+        });
+        amenities.forEach(item => {
+            if(item.checked){
+                review.innerHTML += "Shared bathroom or amenities: "+item.value + "<br>";
+            }
+            
+        });
+        review.innerHTML +=  "Room Type: " +room.options[room.selectedIndex].text + "<br>"
+        genderpref.forEach(item => {
+            if(item.checked){
+                review.innerHTML += "Roomate Preference "+item.value + "<br>";
+            }  
+        });
+        review.innerHTML += `Roomate name: ${mate.value}<br>
+        Date arrival: ${arrive.value}<br>`
+        
     })
+
+
 }
+
 
        
