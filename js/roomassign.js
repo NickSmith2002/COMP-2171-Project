0804f28dcdbc4e5699b6228bb5674895344140c7
NickSmith2002 <114614444+NickSmith2002@users.noreply.github.com>
2022-12-01 02:30:45 -0500

    Update roomassign.js

--- js/roomassign.js
+++ js/roomassign.js
@@ -1,7 +1,6 @@
 window.onload = function(){
     const submitbutton = document.querySelector(".submit")
-    const singleroom = document.getElementById("#single")
-    const doubleroom = document.getElementById("#double")
     const roomnumber = document.getElementById("#roomnum")
-    const residentid = document.getElementById("#residentid")
+    const residentid = document.getElementById("#residentid1")
+    const residentid2 = document.getElementById("#residentid2")
 }
