cf4bef786348ce54d4c69d72289be0a339426a5a
NickSmith2002 <114614444+NickSmith2002@users.noreply.github.com>
2022-11-30 23:22:30 -0500

    Add files via upload

---
+++ js/roomassign.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+    const submitbutton = document.querySelector(".submit")
+    const singleroom = document.getElementById("#single")
+    const doubleroom = document.getElementById("#double")
+    const roomnumber = document.getElementById("#roomnum")
+    const residentid = document.getElementById("#residentid")
+}
