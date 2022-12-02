998180df431bc9190aa0475ac69ef376fe145c70
Kareem Ellis <kareemellis55@gmail.com>
2022-12-01 19:29:00 -0500

    Implemented Application Submission

--- classes/ApplicationModel.class.php
+++ classes/ApplicationModel.class.php
@@ -106,39 +106,20 @@ class ApplicationModel extends DB {
                                            WHERE Applicants.Status = 'Accepted';");
     }
 
-    protected function addApplication($firstName, $lastName, $middleInitial, $DOB, $nationality, 
-                                      $gender, $maritialStatus, $familyType, $homeAddress, $mailingAddress, 
-                                      $emailAddress, $idNumber, $contactName, $contactRelationship, 
-                                      $contactTelephone, $contactAddress, $contactEmail, $levelOfStudy, 
-                                      $yearOfStudy, $programmeName, $facultyName, $nameOfSchool, 
-                                      $roomType, $roommatePref) {
+    protected function addApplication($firstName, $lastName, $middleInitial, $nationality, 
+                                      $gender,
+                                      $emailAddress, $idNumber, $roommatePref) {
         //Add default status of Pending to status
-        $stmt = $this->connect()->prepare("INSERT INTO Applicants (Status, First Name, Last Name, Middle Initial, DOB, Nationality, Gender, Maritial Status, Family Type, Home Address, Mailing Address, Email Address, ID Number, Contact Name, Contact Relationship, Contact Telephone, Contact Address, Contact Email, Level of Study, Year of Study, Programme Name, Faculty Name, Name of School, Room Type, Roommate Preference) 
-                VALUES (pending, :firstName, :lastName, :middleInitial, :DOB, :nationality, :gender, :maritialStatus, :familyType, :homeAddress, :mailingAddress, :emailAddress, :idNumber, :contactName, :contactRelationship, :contactTelephone, :contactAddress, :contactEmail, :levelOfStudy, :yearOfStudy, :programmeName, :facultyName, :nameOfSchool, :roomType, :roommatePref)");
+        $stmt = $this->connect()->prepare("INSERT INTO Applicants (Status, `First Name`, `Last Name`, `Middle Initial`, `Nationality`, Gender, `Email Address`, `ID Number`, `Roommate Preference`) 
+                VALUES ('Pending', :firstName, :lastName, :middleInitial, :nationality, :gender, :emailAddress, :idNumber, :roommatePref)");
         
         $stmt->bindValue(':firstName', $firstName, PDO::PARAM_STR);
         $stmt->bindValue(':lastName', $lastName, PDO::PARAM_STR);
         $stmt->bindValue(':middleInitial', $middleInitial, PDO::PARAM_STR);
-        $stmt->bindValue(':DOB', $DOB, PDO::PARAM_STR);
         $stmt->bindValue(':nationality', $nationality, PDO::PARAM_STR);
         $stmt->bindValue(':gender', $gender, PDO::PARAM_STR);
-        $stmt->bindValue(':maritialStatus', $maritialStatus, PDO::PARAM_STR);
-        $stmt->bindValue(':familyType', $familyType, PDO::PARAM_STR);
-        $stmt->bindValue(':homeAddress', $homeAddress, PDO::PARAM_STR);
-        $stmt->bindValue(':mailingAddress', $mailingAddress, PDO::PARAM_STR);
         $stmt->bindValue(':emailAddress', $emailAddress, PDO::PARAM_STR);
         $stmt->bindValue(':idNumber', $idNumber, PDO::PARAM_STR);
-        $stmt->bindValue(':contactName', $contactName, PDO::PARAM_STR);
-        $stmt->bindValue(':contactRelationship', $contactRelationship, PDO::PARAM_STR);
-        $stmt->bindValue(':contactTelephone', $contactTelephone, PDO::PARAM_STR);
-        $stmt->bindValue(':contactAddress', $contactAddress, PDO::PARAM_STR);
-        $stmt->bindValue(':contactEmail', $contactEmail, PDO::PARAM_STR);
-        $stmt->bindValue(':levelOfStudy', $levelOfStudy, PDO::PARAM_STR);
-        $stmt->bindValue(':yearOfStudy', $yearOfStudy, PDO::PARAM_STR);
-        $stmt->bindValue(':programmeName', $programmeName, PDO::PARAM_STR);
-        $stmt->bindValue(':facultyName', $facultyName, PDO::PARAM_STR);
-        $stmt->bindValue(':nameOfSchool', $nameOfSchool, PDO::PARAM_STR);
-        $stmt->bindValue(':roomType', $roomType, PDO::PARAM_STR);
         $stmt->bindValue(':roommatePref', $roommatePref, PDO::PARAM_STR);
 
         $stmt->execute();
